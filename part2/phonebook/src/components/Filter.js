674b95979fa2187de0e82bfed8fcd53ea509956c
leksoid <aliaksei.taran@gmail.com>
2020-12-19 22:33:16 -0500

    Use express backend for data fetching

--- part2/phonebook/src/components/Filter.js
+++ part2/phonebook/src/components/Filter.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Filter = ({filter, handleFilterChange}) => {
     return (
         <div>
-            Search: <input id='search' value={filter} onChange={handleFilterChange}/>
+            Search: <input id='search' value={filter} onChange={handleFilterChange} autoComplete="off"/>
         </div>
     )
 }
--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -4,10 +4,10 @@ const PersonForm = (props) => {
     return (
         <form id='addPersonForm' onSubmit={props.addPerson}>
         <div>
-          name: <input id='nameField' value={props.newName} onChange={props.handleNameChange}/>
+          name: <input id='nameField' value={props.newName} onChange={props.handleNameChange} autoComplete="off"/>
         </div>
         <div>
-          phone number: <input id='numberField' value={props.newPhone} onChange={props.handlePhoneChange}/>
+          phone number: <input id='numberField' value={props.newPhone} onChange={props.handlePhoneChange} autoComplete="off"/>
         </div>
         <div>
           <button id='submitForm' type='submit'>add</button>
